29ed7d5a8ef5826d072780c8719cf32e6b10a37d
ddpisces <ddpisces@hotmail.com>
2013-10-27 05:17:06 -0700

    change deprecate properties

--- OnlineEducation/NVUIGradientButton/NVUIGradientButton.m
+++ OnlineEducation/NVUIGradientButton/NVUIGradientButton.m
@@ -131,11 +131,11 @@ static CGGradientRef NVCGGradientCreate(CGColorRef startColor, CGColorRef endCol
 	
 	// Label
 	_titleLabel = [[UILabel alloc] init];
-	_titleLabel.textAlignment = UITextAlignmentCenter;
-	_titleLabel.lineBreakMode = UILineBreakModeMiddleTruncation;
+	_titleLabel.textAlignment = NSTextAlignmentCenter;
+	_titleLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
 	_titleLabel.numberOfLines = 0;
 	_titleLabel.font = [UIFont boldSystemFontOfSize:15.0];
-	_titleLabel.minimumFontSize = 12.0;
+	_titleLabel.minimumScaleFactor = 12.0;
 	_titleLabel.shadowOffset = CGSizeMake(0, -1);
 	
 	if (![[self class] conformsToProtocol:@protocol(UIAppearance)])
