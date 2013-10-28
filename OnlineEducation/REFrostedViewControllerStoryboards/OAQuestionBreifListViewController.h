c52042967fc0b107382ea8aa433b93ae4c65b598
zz cienet <zzcienet@mbp.local>
2013-10-28 17:47:10 +0800

    add brief question list view controller

---
+++ OnlineEducation/REFrostedViewControllerStoryboards/OAQuestionBreifListViewController.h
@@ -0,0 +1,13 @@
+//
+//  OAQuestionBreifListViewController.h
+//  REFrostedViewControllerStoryboards
+//
+//  Created by zz cienet on 10/28/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAQuestionBreifListViewController : UITableViewController
+
+@end
